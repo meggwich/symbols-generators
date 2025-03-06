66fe38d389b158c81cea38a3ba4e9b53ce4b3ef7
Mikita <dixtry3826@gmail.com>
2025-03-06 16:45:28 +0100

    generator

--- src/index.js
+++ src/index.js
@@ -23,23 +23,9 @@ class Team {
         }
     }
 
-    [Symbol.iterator]() {
-        let index = 0;
-        const members = this.members;
-
-        return {
-            next() {
-                if (index < members.length) {
-                    return {
-                        value: members[index++],
-                        done: false,
-                    }
-                } else {
-                    return {
-                        done: true,
-                    }
-                }
-            }
+    *[Symbol.iterator]() {
+        for (const member of this.members) {
+        yield member; 
         }
     }
 }
@@ -54,4 +40,4 @@ team.add(new Character('Маг', 'Wizard', 40, 1, 50, 5));
 
 for (const character of team) {
     console.log(character);
-}
+  }
--- src/js/Team.js
+++ src/js/Team.js
@@ -14,23 +14,9 @@ class Team {
         }
     }
 
-    [Symbol.iterator]() {
-        let index = 0;
-        const members = this.members;
-
-        return {
-            next() {
-                if (index < members.length) {
-                    return {
-                        value: members[index++],
-                        done: false,
-                    }
-                } else {
-                    return {
-                        done: true,
-                    }
-                }
-            }
+    *[Symbol.iterator]() {
+        for (const member of this.members) {
+        yield member; 
         }
     }
 }
